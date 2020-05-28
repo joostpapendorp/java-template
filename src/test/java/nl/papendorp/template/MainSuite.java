351f509ba76e31fa187a5efaff5a7417d25a16f9
Joost Papendorp <jpapendorp@vxcompany.nl>
2020-05-28 22:10:11 +0200

    [JRP 2] Hardened the example with edge cases. Formatting.

--- src/test/java/nl/papendorp/template/MainSuite.java
+++ src/test/java/nl/papendorp/template/MainSuite.java
@@ -2,7 +2,6 @@ package nl.papendorp.template;
 
 import nl.papendorp.testing.shared.logging.AppenderSpy;
 import org.apache.logging.log4j.core.Logger;
-import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import static org.apache.logging.log4j.LogManager.getLogger;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName( "This app should" )
 class MainSuite
@@ -22,24 +22,39 @@ class MainSuite
 	{
 		appender = new AppenderSpy();
 		((Logger) getLogger( Main.class ))
-				.addAppender( appender );
+			.addAppender( appender );
 	}
 
 	@Test
-	@DisplayName( "log its command line arguments" )
+	@DisplayName( "log its command line arguments." )
 	void app_logs_arguments()
 	{
 		new Main().main( "Test" );
 		Main.main( "test", "suite" );
 
-		assertThat( appender.messages(), contains( "Hello, Test", "Hello, test and suite" ) );
+		assertThat( appender.messages(), contains( "Hello, Test!", "Hello, test and suite!" ) );
+	}
+
+	@Test
+	@DisplayName( "provide default when no arguments are supplied." )
+	void provide_default()
+	{
+		Main.main();
+		assertThat( appender.messages(), contains( "Hello, there!" ) );
+	}
+
+	@Test
+	@DisplayName( "reject nulls." )
+	void reject_nulls()
+	{
+		assertThrows( IllegalArgumentException.class, () -> Main.main( null ) );
 	}
 
 	@AfterEach
 	void tearDown()
 	{
 		((Logger) getLogger( Main.class ))
-				.removeAppender( appender );
+			.removeAppender( appender );
 		appender.stop();
 	}
 }
